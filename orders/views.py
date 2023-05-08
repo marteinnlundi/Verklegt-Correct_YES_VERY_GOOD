82db6f1d907dd116049d584dc539f5b23ab25291
marteinnlundi <112634382+marteinnlundi@users.noreply.github.com>
2023-05-08 14:01:13 +0000

    add to cart og hreinsa hana

--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ import logging
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import PaymentForm
+from products.models import Products
 
 def cart_view(request):
     return render(request, 'cart.html')
@@ -36,7 +37,25 @@ def checkout_view(request):
     return render(request, 'checkout.html', {'form': form})
 
 
+def add_to_cart(request, product_id):
+    print(product_id)
+    product = Products.objects.get(id=product_id)
+    cart = request.session.get('cart', {})
+    cart_item = cart.get(str(product_id), {'quantity': 0, 'price': str(product.price)})
+    cart_item['name'] = product.name # add product name to cart_item dictionary
+    cart_item['quantity'] += 1
+    cart_item['price'] = str(product.price * cart_item['quantity'])
+    cart[str(product_id)] = cart_item
+    request.session['cart'] = cart
+    messages.success(request, f"{product.name} added to cart.")
+    return redirect('cart')
+
+
 def confirmation_view(request):
     logging.debug('Rendering confirmation page')
     return render(request, 'confirmation.html')
 
+def clear_cart(request):
+    request.session['cart'] = {}
+    messages.success(request, "Cart cleared.")
+    return redirect('cart')
