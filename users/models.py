05be27899df889a8243fca0f0074877aa986ae7f
Ágústa Björk <agusta20@ru.is>
2023-05-08 14:25:00 +0000

    Offers

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 
+
 import users
 
 class Profile(models.Model):
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render,redirect
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
+
+
 import logging
 logger = logging.getLogger(__name__)
 
