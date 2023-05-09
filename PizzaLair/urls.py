e8a8e3541e2402387cf11f85d9bff2efb9b1c7c6
GElisabet <gudbjorges21@ru.is>
2023-05-09 13:54:37 +0000

    signin + profilemynd

--- PizzaLair/urls.py
+++ PizzaLair/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from users.views import home_view
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 # admin localhost:8000/admin
@@ -32,4 +32,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
 
-    ] 
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -8,7 +8,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     profile_picture = models.ImageField(upload_to='user_images')
-    email = models.EmailField()
+    email = models.EmailField( max_length=254)
     password = models.CharField(max_length=100)
 
 
